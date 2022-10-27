43f42ade2001ee4730d7d0cde663bb56814e90d3
Zachary Déziel <zacharydeziel@pop-os.localdomain>
2022-10-27 11:44:03 -0700

    Add multiple h3 resolution to facilitate navigation

---
+++ data/dbt_project/models/h3_r4.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table',
+indexes=[  { 'columns': ['h3_id']}, { 'columns': ['geom'], 'type': 'gist'},
+])}}
+
+with h3_ids as (
+select h3_polyfill(geom, 4) h3_id from {{ref('pois_envelope')}}
+)
+select h3_id, st_setsrid(h3_h3index_to_geoboundary(h3_id), 4326) geom from h3_ids
--- data/dbt_project/models/h3_r4_access.sql
+++ data/dbt_project/models/h3_r4_access.sql
@@ -5,12 +5,12 @@ indexes=[  { 'columns': ['h3_id']}, { 'columns': ['geom'], 'type': 'gist'},
 {%
 set grps = dbt_utils.get_column_values(table=ref('pois_groups'), column='group_name') %}
 
-with pois_h3_r6 as (
-    select *, h3_geo_to_h3index(geom, 6) as h3index
+with pois_h3_r4 as (
+    select *, h3_geo_to_h3index(geom, 4) as h3index
     from {{ref('pois')}}
 ),
      count_by_type as (
-         select h3index, group_name, count(*) as count from pois_h3_r6 group by h3index, group_name
+         select h3index, group_name, count(*) as count from pois_h3_r4 group by h3index, group_name
      ),
      jsonb_magic as (
          select h3index, jsonb_object_agg(group_name, count) as type_counts from count_by_type group by h3index
@@ -21,7 +21,7 @@ select h3index h3_id,
     {% endfor %}
     b.geom
 from jsonb_magic a
-    join {{ref ('h3_r6')}} b
+    join {{ref ('h3_r4')}} b
 on a.h3index = b.h3_id
 union
 select h3_id,
@@ -29,5 +29,5 @@ select h3_id,
     0 {{ grp }},
     {% endfor %}
     geom
-from {{ref ('h3_r6')}}
+from {{ref ('h3_r4')}}
 where h3_id not in (select h3index from count_by_type)
